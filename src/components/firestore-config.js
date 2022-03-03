94da58af8e89d628d93176b56e6ea3c0ca4d4f3a
DamianLtz <damianlaterza@gmail.com>
2022-03-03 17:39:35 -0300

    v1.5.1

--- src/components/firestore-config.js
+++ src/components/firestore-config.js
@@ -2,19 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-console.log(process.env.API_KEY)
-console.log(process.env.AUTH_DOMAIN)
-console.log(process.env.PROYECT_ID)
-console.log(process.env.STORAGE_BUCKET)
-console.log(process.env.MESSAGING_SENDER_ID)
-console.log(process.env.APP_ID)
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROYECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: "AIzaSyCBlD-5R2DbOeX_WyyPul7D6HispqlAg04",
+  authDomain: "personalecommerce-d29f3.firebaseapp.com",
+  projectId: "personalecommerce-d29f3",
+  storageBucket: "personalecommerce-d29f3.appspot.com",
+  messagingSenderId: "953964934750",
+  appId: "1:953964934750:web:5196e1a966b53305e4e6cf"
 };
 
 // Initialize Firebase
